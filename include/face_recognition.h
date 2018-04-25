04621352ed0661a888d1fa0f444de12509cbe1c2
Leah Holden <leah.holden@tufts.edu>
2018-04-25 15:38:05 -0400

    Sketchy: not compiled, may have bugs. Many many bugs. Added the class declaration for face_recognition

---
+++ include/face_recognition.h
@@ -0,0 +1,23 @@
+#include "opencv2/core.hpp"
+#include "opencv2/face.hpp"
+#include "opencv2/highgui.hpp"
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace cv;
+using namespace cv::face;
+using namespace std;
+
+class Face_Recognition {
+	private:
+	   string model_path = "/home/turtlebot/catkin_ws/src/greeter_robot/data/recognizer_model.xml";
+	   static void read_csv(const string& filename, vector<Mat>& images, vector<int>& labels, char separator);
+	public:
+	   void init(vector<Mat> imgs, vector<int> labels);
+	   void update_model(vector<Mat> imgs, vector<int> labels);
+	   string identify(Mat img);
+};
+	   
+
---
+++ src/face_recognition.cpp
@@ -0,0 +1,37 @@
+#include "opencv2/core.hpp"
+#include "opencv2/face.hpp"
+#include "opencv2/highgui.hpp"
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include "face_recognition.h"
+
+using namespace cv;
+using namespace cv::face;
+using namespace std;
+
+static void read_csv(const string& filename, vector<Mat>& images, vector<int>& labels, char separator = ';') {
+    std::ifstream file(filename.c_str(), ifstream::in);
+    if (!file) {
+        string error_message = "No valid input file was given, please check the given filename.";
+        CV_Error(Error::StsBadArg, error_message);
+    }
+    string line, path, classlabel;
+    while (getline(file, line)) {
+        stringstream liness(line);
+        getline(liness, path, separator);
+        getline(liness, classlabel);
+        if(!path.empty() && !classlabel.empty()) {
+            images.push_back(imread(path, 0));
+            labels.push_back(atoi(classlabel.c_str()));
+        }
+    }
+}
+
+void init(vector<Mat> imgs, vector<int> labels) {
+    Ptr<LBPHFaceRecognizer> model = LCPHFaceRecognizer::create();
+    model->train(imgs, labels);
+    model->save(model->model_path); 
+}

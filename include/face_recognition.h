574a5cb899e8d882f37f6270d237dc00c93daa2a
Anne Oursler <anneourlser@gmail.com>
2018-04-29 15:43:50 -0400

    moved face recognition to it's own class

--- include/face_recognition.h
+++ include/face_recognition.h
@@ -8,16 +8,12 @@
 #include <sstream>
 
 using namespace cv;
-//using namespace cv::face;
 using namespace std;
 
 class Face_Recognition {
 	private:
-	   cv::Ptr<cv::FaceRecognizer> model;//Ptr<LBPHFaceRecognizer> model;
-	   string model_path = "/home/turtlebot/catkin_ws/src/greeter_robot/data/recognizer_model.xml";
-	   void read_csv(const string& filename, vector<Mat>& images, vector<int>& labels, char separator);
+	   Mat norm_0_255(InputArray _src);
+           void read_csv(const string& filename, vector<Mat>& images, vector<int>& labels, char separator);
 	public:
-	   Face_Recognition(vector<Mat> imgs, vector<int> labels);
-	   void update_model(vector<Mat> imgs, vector<int> labels);
-	   int identify(Mat img);
+	   int recognize_faces();
 };
--- src/face_recognition.cpp
+++ src/face_recognition.cpp
@@ -1,20 +1,58 @@
-#include "opencv2/core/core.hpp"
+#include "face_detection.h"
+#include "face_recognition.h"
 #include <opencv2/contrib/contrib.hpp>
-//#include "opencv2/face/face.hpp"
+
+/*
+ * Copyright (c) 2011. Philipp Wagner <bytefish[at]gmx[dot]de>.
+ * Released to public domain under terms of the BSD Simplified license.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of the organization nor the names of its contributors
+ *     may be used to endorse or promote products derived from this software
+ *     without specific prior written permission.
+ *
+ *   See <http://www.opensource.org/licenses/bsd-license>
+ */
+#include "opencv2/core/core.hpp"
+#include "opencv2/contrib/contrib.hpp"
 #include "opencv2/highgui/highgui.hpp"
 
 #include <iostream>
 #include <fstream>
 #include <sstream>
 
-#include "face_recognition.h"
-
 using namespace cv;
-//using namespace cv::face;
 using namespace std;
 
+double IMAGE_WIDTH = 100;
+double IMAGE_HEIGHT = 100;
+char csv_separator = ';';
 
-void Face_Recognition::read_csv(const string& filename, vector<Mat>& images, vector<int>& labels, char separator = ';') {
+Mat Face_Recognition::norm_0_255(InputArray _src) {
+    Mat src = _src.getMat();
+    // Create and return normalized image:
+    Mat dst;
+    switch(src.channels()) {
+    case 1:
+        cv::normalize(_src, dst, 0, 255, NORM_MINMAX, CV_8UC1);
+        break;
+    case 3:
+        cv::normalize(_src, dst, 0, 255, NORM_MINMAX, CV_8UC3);
+        break;
+    default:
+        src.copyTo(dst);
+        break;
+    }
+    return dst;
+}
+
+void Face_Recognition::read_csv(const string& filename, vector<Mat>& images, vector<int>& labels, char separator) {
     std::ifstream file(filename.c_str(), ifstream::in);
     if (!file) {
         string error_message = "No valid input file was given, please check the given filename.";
@@ -32,17 +70,74 @@ void Face_Recognition::read_csv(const string& filename, vector<Mat>& images, vec
     }
 }
 
-Face_Recognition::Face_Recognition(vector<Mat> imgs, vector<int> labels) {
-    model = cv::createEigenFaceRecognizer(); //LCPHFaceRecognizer::create();
-    model->train(imgs, labels);
-    model->save(model_path); 
-}
+int Face_Recognition::recognize_faces() {
+    //string output_folder = ".";
+    string fn_csv = "/home/turtlebot/catkin_ws/src/greeter_robot/data/faces.csv";
+    // These vectors hold the images and corresponding labels.
+    vector<Mat> images;
+    vector<int> labels;
+    // Read in the data. This can fail if no valid
+    // input filename is given.
+    try {
+        read_csv(fn_csv, images, labels, csv_separator);
+    } catch (cv::Exception& e) {
+        cerr << "Error opening file \"" << fn_csv << "\". Reason: " << e.msg << endl;
+        // nothing more we can do
+        exit(1);
+    }
+    // Quit if there are not enough images for this demo.
+    if(images.size() <= 1) {
+        string error_message = "This demo needs at least 2 images to work. Please add more images to your data set!";
+        CV_Error(CV_StsError, error_message);
+    }
+    // Get the height from the first image. We'll need this
+    // later in code to reshape the images to their original
+    // size:
+    int height = images[0].rows;
 
-void Face_Recognition::update_model(vector<Mat> imgs, vector<int> labels) {
-    model->update(imgs, labels);
-    model->save(model_path);
-}
+    //TODO: GET IMAGE TO RECOGNIZE FROM FACIAL DETECTION AND CAMERA
+    // The following lines simply get the last images from
+    // your dataset and remove it from the vector. This is
+    // done, so that the training data (which we learn the
+    // cv::FaceRecognizer on) and the test data we test
+    // the model with, do not overlap.
+    Mat testSample = images[images.size() - 1];
+    int testLabel = labels[labels.size() - 1];
+    images.pop_back();
+    labels.pop_back();
 
-int Face_Recognition::identify(Mat img) {
-    return model->predict(img);
+    // The following lines create an Eigenfaces model for
+    // face recognition and train it with the images and
+    // labels read from the given CSV file.
+    // This here is a full PCA, if you just want to keep
+    // 10 principal components (read Eigenfaces), then call
+    // the factory method like this:
+    //
+    //      cv::createEigenFaceRecognizer(10);
+    //
+    // If you want to create a FaceRecognizer with a
+    // confidence threshold (e.g. 123.0), call it with:
+    //
+    //      cv::createEigenFaceRecognizer(10, 123.0);
+    //
+    // If you want to use _all_ Eigenfaces and have a threshold,
+    // then call the method like this:
+    //
+    //      cv::createEigenFaceRecognizer(0, 123.0);
+    //
+    //Create a Faceial Recognizer
+    Ptr<FaceRecognizer> model = createEigenFaceRecognizer();
+    //Train the Faceial Recognizer
+    model->train(images, labels);
+
+    // predict the face in an image with the confidence
+    int predictedLabel = -1;
+    double confidence = 0.0;
+    model->predict(testSample, predictedLabel, confidence);
+
+    ROS_INFO("The predicted label is %i, which corresponds to a name in the csv \n", predictedLabel);
+    ROS_INFO("This was predicted with a confidence level of %lf \n", confidence);
+    //TODO: Write code to get name given label    
+
+    return predictedLabel;
 }

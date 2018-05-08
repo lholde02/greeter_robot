937603aa89d3e33c400cfbabaad037bef8eeb0f3
name <test@example.com>
2018-05-08 17:25:35 -0400

    welcome messages are created and go into welcome folder

--- data/createwelcome.sh
+++ data/createwelcome.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo Createing a welcome message for $1
-cat > /home/turtlebot/catkin_ws/src/greeter_robot/data/welcome$1.sh <<EOF
+cat > /home/turtlebot/catkin_ws/src/greeter_robot/data/welcomemessages/welcome$1.sh <<EOF
 #!/bin/bash
 rosrun sound_play say.py "Welcome to Halligan $1!"
 EOF
-chmod +x /home/turtlebot/catkin_ws/src/greeter_robot/data/welcome$1.sh
+chmod +x /home/turtlebot/catkin_ws/src/greeter_robot/data/welcomemessages/welcome$1.sh
 
